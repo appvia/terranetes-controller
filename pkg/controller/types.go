1158aceeedb67191bba0338531c7c0849795f2e2
Rohith Jayawardene <gambol99@gmail.com>
2022-05-04 09:30:11 +0100

    - removing the name from licence

--- pkg/controller/types.go
+++ pkg/controller/types.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022  Rohith Jayawardene <gambol99@gmail.com>
+ * Copyright (C) 2022  Appvia Ltd <info@appvia.io>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
