407842689fb7067a1745090eb25da50f7cf155ab
Rohith Jayawardene <gambol99@gmail.com>
2022-07-06 18:08:45 +0100

    [TESTING] - Adding Unit Tests for CLI (#216)
    Adding a unit test pattern for the CLI

--- pkg/cmd/tnctl/config/sources_test.go
+++ pkg/cmd/tnctl/config/sources_test.go
@@ -18,19 +18,134 @@
 package config
 
 import (
-	"testing"
+	"bytes"
+	"os"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 
 	"github.com/appvia/terraform-controller/pkg/cmd"
 )
 
-func TestNewSourcesCommand(t *testing.T) {
-	factory, err := cmd.NewFactory(genericclioptions.NewTestIOStreamsDiscard())
-	assert.NoError(t, err)
-	assert.NotNil(t, factory)
+var _ = Describe("Sources Command", func() {
+	var factory cmd.Factory
+	var streams genericclioptions.IOStreams
+	var stdout *bytes.Buffer
+	var rerr error
+	var c *cobra.Command
+	var configFile *os.File
 
-	c := NewSourcesCommand(factory)
-	assert.NotNil(t, c)
-}
+	BeforeEach(func() {
+		var err error
+		streams, _, stdout, _ = genericclioptions.NewTestIOStreams()
+		factory, err = cmd.NewFactory(streams)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(factory).NotTo(BeNil())
+
+		configFile, err = os.CreateTemp(os.TempDir(), "config-sources.XXXXX")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(configFile).NotTo(BeNil())
+		Expect(configFile.Close()).To(Succeed())
+		os.Setenv(cmd.ConfigPathEnvName, configFile.Name())
+
+		c = NewSourcesCommand(factory)
+	})
+
+	AfterEach(func() {
+		os.Remove(configFile.Name())
+	})
+
+	When("adding a new source", func() {
+		When("source does not exist", func() {
+			BeforeEach(func() {
+				os.Args = []string{"sources", "add", "https://foo"}
+				rerr = c.Execute()
+			})
+
+			It("should have updated the configuration", func() {
+				Expect(rerr).NotTo(HaveOccurred())
+
+				content, err := os.ReadFile(configFile.Name())
+				Expect(err).NotTo(HaveOccurred())
+				Expect(content).ToNot(BeEmpty())
+				Expect(string(content)).To(Equal("sources:\n- https://foo\n"))
+
+				Expect(stdout.String()).To(ContainSubstring("Successfully saved configuration"))
+			})
+		})
+
+		When("source already exists", func() {
+			BeforeEach(func() {
+				os.WriteFile(configFile.Name(), []byte("sources:\n- https://foo\n"), 0644)
+				os.Args = []string{"sources", "add", "https://foo"}
+				rerr = c.Execute()
+			})
+
+			It("should not have updated the configuration", func() {
+				Expect(rerr).NotTo(HaveOccurred())
+
+				content, err := os.ReadFile(configFile.Name())
+				Expect(err).NotTo(HaveOccurred())
+				Expect(content).ToNot(BeEmpty())
+				Expect(string(content)).To(Equal("sources:\n- https://foo\n"))
+
+				Expect(stdout.String()).To(ContainSubstring("Source already exists"))
+			})
+		})
+	})
+
+	When("deleting a source from the configuration", func() {
+		BeforeEach(func() {
+			os.WriteFile(configFile.Name(), []byte("sources:\n- https://foo\n"), 0644)
+
+			os.Args = []string{"sources", "remove", "https://foo"}
+			rerr = c.Execute()
+		})
+
+		It("should have deleted the source", func() {
+			Expect(rerr).NotTo(HaveOccurred())
+
+			content, err := os.ReadFile(configFile.Name())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(content).ToNot(BeEmpty())
+			Expect(string(content)).To(Equal("{}\n"))
+
+			Expect(stdout.String()).To(ContainSubstring("Successfully saved configuration"))
+		})
+	})
+
+	When("listing the sources from the configuration", func() {
+		BeforeEach(func() {
+			os.WriteFile(configFile.Name(), []byte("sources:\n- https://foo\n"), 0644)
+
+			os.Args = []string{"sources", "list"}
+		})
+
+		When("config does not exist", func() {
+			BeforeEach(func() {
+				os.Remove(configFile.Name())
+				rerr = c.Execute()
+			})
+
+			It("should show missing config", func() {
+				Expect(rerr).NotTo(HaveOccurred())
+				Expect(stdout.String()).To(ContainSubstring("No configuration found at"))
+			})
+		})
+
+		When("config exists", func() {
+			BeforeEach(func() {
+				rerr = c.Execute()
+			})
+
+			It("list the sources", func() {
+				expected := "You currently have the following sources active\n- https://foo\n"
+
+				Expect(rerr).NotTo(HaveOccurred())
+				Expect(stdout.String()).To(Equal(expected))
+			})
+		})
+	})
+})
