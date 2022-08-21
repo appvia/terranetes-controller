40051cef8e7fc655fa578284b8c2bb5e5ef064aa
Rohith Jayawardene <gambol99@gmail.com>
2022-08-21 21:39:31 +0100

    [CLI] - Setting a Default Namespace (#343)
    Adding a default namespace for logs and describe command

--- pkg/cmd/tnctl/describe/describe.go
+++ pkg/cmd/tnctl/describe/describe.go
@@ -78,7 +78,7 @@ func NewCommand(factory cmd.Factory) *cobra.Command {
 
 	flags := c.Flags()
 	flags.BoolVar(&options.ShowPassedChecks, "show-passed-checks", true, "Indicates we should show passed checks")
-	flags.StringVarP(&options.Namespace, "namespace", "n", "", "Namespace of the resource/s")
+	flags.StringVarP(&options.Namespace, "namespace", "n", "default", "Namespace of the resource/s")
 
 	cmd.RegisterFlagCompletionFunc(c, "namespace", cmd.AutoCompleteNamespaces(factory))
 
