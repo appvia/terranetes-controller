3987290cb237b98a7d279a4c0b07bf72f9f919e7
Rohith Jayawardene <gambol99@gmail.com>
2022-09-01 09:26:28 +0100

    [E2E] - Adding Google Project ID
    Adding the project id variable to the E2E google terraform manifests

--- test/e2e/assets/terraform/google/main.tf
+++ test/e2e/assets/terraform/google/main.tf
@@ -2,7 +2,8 @@ module "bucket" {
   source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version = "~> 1.3"
 
-  name     = var.bucket
-  location = var.location
+  name        = var.bucket
+  location    = var.location
+  project_id  = var.project_id
 }
 
