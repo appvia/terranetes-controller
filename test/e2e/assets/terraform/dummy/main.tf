6c350e6016607a879033f3d327bf497287563ce8
Rohith Jayawardene <gambol99@gmail.com>
2023-05-10 16:43:33 +0100

    [E2E] - Switching Dummy Module Requirements
    The AWS providers is too large; choosing a smaller provider

--- test/e2e/assets/terraform/dummy/main.tf
+++ test/e2e/assets/terraform/dummy/main.tf
@@ -8,9 +8,9 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.62"
+    time = {
+      source = "hashicorp/time"
+      version = "0.9.1"
     }
   }
 }
