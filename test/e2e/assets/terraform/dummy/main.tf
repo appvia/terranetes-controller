2bcfc84c8f5f0be42972b60511714c510724845a
Rohith Jayawardene <gambol99@gmail.com>
2023-05-10 10:15:02 +0100

    [E2E] - Dummy Terraform Module
    Updating the terraform module to have a output and terraform settings

--- test/e2e/assets/terraform/dummy/main.tf
+++ test/e2e/assets/terraform/dummy/main.tf
@@ -1,5 +1,21 @@
 
-resource "random_integer" "nothing" {
+resource "random_integer" "this" {
   min     = 1
   max     = 99999
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.62"
+    }
+  }
+}
+
+output "number" {
+  description = "The random number generated"
+  value = random_integer.this.result
 }
